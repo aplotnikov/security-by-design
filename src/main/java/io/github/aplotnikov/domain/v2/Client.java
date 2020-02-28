61dcc779afba970e458c02bfafa9721f36a583fb
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2020-02-29 00:13:26 +0100

    Add Password value object and unit tests

--- src/main/java/io/github/aplotnikov/domain/v2/Client.java
+++ src/main/java/io/github/aplotnikov/domain/v2/Client.java
@@ -1,15 +1,71 @@
 package io.github.aplotnikov.domain.v2;
 
-class Client {
+import static java.util.Objects.requireNonNull;
 
-    ClientId id;
+import java.util.Objects;
 
-    FirstName firstName;
+final class Client {
 
-    SecondName secondName;
+    private final ClientId id;
 
-    String password;
+    private final FirstName firstName;
 
-    PersonalId personalId;
+    private final SecondName secondName;
 
+    private final Password password;
+
+    private final PersonalId personalId;
+
+    Client(ClientId id, FirstName firstName, SecondName secondName, Password password, PersonalId personalId) {
+        this.id = requireNonNull(id, "Client id can not be null");
+        this.firstName = requireNonNull(firstName, "First name can not be null");
+        this.secondName = requireNonNull(secondName, "Second name can not be null");
+        this.password = requireNonNull(password, "Password can not be null");
+        this.personalId = requireNonNull(personalId, "Personal id can not be null");
+    }
+
+    ClientId getId() {
+        return id;
+    }
+
+    FirstName getFirstName() {
+        return firstName;
+    }
+
+    SecondName getSecondName() {
+        return secondName;
+    }
+
+    Password getPassword() {
+        return password;
+    }
+
+    PersonalId getPersonalId() {
+        return personalId;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        Client client = (Client) other;
+        return Objects.equals(id, client.id) &&
+            Objects.equals(personalId, client.personalId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, personalId);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{id=" + id + '}';
+    }
 }
---
+++ src/test/groovy/io/github/aplotnikov/domain/v2/PasswordSpec.groovy
@@ -0,0 +1,91 @@
+package io.github.aplotnikov.domain.v2
+
+import org.apache.commons.lang3.SerializationUtils
+
+import spock.lang.Specification
+
+class PasswordSpec extends Specification {
+
+    void 'should be impossible to create password when value is null'() {
+        when:
+            Password.of(null)
+        then:
+            NullPointerException exception = thrown()
+            exception.message == 'Password can not be null'
+    }
+
+    void 'should be impossible to create password when value is lower than 8 symbols'() {
+        when:
+            Password.of(value.chars)
+        then:
+            IllegalStateException exception = thrown()
+            exception.message == 'Password can not contain less than 8 symbols'
+        where:
+            value << [
+                '',
+                ' ',
+                ' '.repeat(7),
+                'A'.repeat(7)
+            ]
+    }
+
+    void 'should be impossible to create password when value is longer than 50 symbols'() {
+        when:
+            Password.of('A'.repeat(51).chars)
+        then:
+            IllegalStateException exception = thrown()
+            exception.message == 'Password can not contain more than 50 symbols'
+    }
+
+    void 'should be impossible to create password when value contains another symbols except latin letters'() {
+        when:
+            Password.of(value.chars)
+        then:
+            IllegalStateException exception = thrown()
+            exception.message == 'Password has to contain only digits and letters'
+        where:
+            value << [
+                'A'.repeat(29) + '!',
+                'A'.repeat(29) + ' ',
+                ' ' + 'A'.repeat(29),
+                'A'.repeat(14) + ' ' + 'A'.repeat(15),
+                '\'\' or 1=1'
+            ]
+    }
+
+    void 'should be possible to create password'() {
+        when:
+            Password password = Password.of('Andrii1234'.chars)
+        then:
+            password.value == 'Andrii1234'.chars
+    }
+
+    void 'should be impossible to extract password more than one time'() {
+        given:
+            Password password = Password.of('Andrii1234'.chars)
+        and:
+            password.value
+        when:
+            password.value
+        then:
+            IllegalStateException exception = thrown()
+            exception.message == 'Password value has already been consumed'
+    }
+
+    void 'should be impossible to see password value through toString method'() {
+        given:
+            Password password = Password.of('Andrii1234'.chars)
+        expect:
+            password.toString() == 'Password{value=\'********\'}'
+    }
+
+    void 'should be impossible to serialize password object'() {
+        given:
+            Password password = Password.of('Andrii1234'.chars)
+        when:
+            SerializationUtils.serialize(password)
+        then:
+            UnsupportedOperationException exception = thrown()
+            exception.message == 'Not allowed operation on password object'
+    }
+}
